d81b80477ca47dd5c73f536386a708962e61ba64
Wendril <dethomassin.etienne@gmail.com>
2019-11-04 18:37:55 +0100

    oops forgot this one

---
+++ Discovery/Core/Inc/robot/robot.h
@@ -0,0 +1,14 @@
+/*
+ * robot_specs.h
+ *
+ *  Created on: Oct 28, 2019
+ *      Author: Wendril
+ */
+
+#ifndef INC_ROBOT_ROBOT_H_
+#define INC_ROBOT_ROBOT_H_
+
+#define	WHEEL_SEPARATION_DISTANCE 0.12345
+
+
+#endif /* INC_ROBOT_ROBOT_H_ */
